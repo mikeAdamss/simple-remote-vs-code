d022018bced606cb1a8c3f7f8bd407fc71bafa5a
Michael Adams <mikelovesbooks@gmail.com>
2020-06-26 23:15:40 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-curl -s https://api.github.com/repos/cdr/code-server/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+export VERSION=`curl -s https://api.github.com/repos/cdr/code-server/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
 wget https://github.com/cdr/code-server/releases/download/$VERSION/code-server-$VERSION-linux-x86_64.tar.gz
 tar -xvzf code-server-$VERSION-linux-x86_64.tar.gz
 cd code-server-$VERSION-linux-x86_64
